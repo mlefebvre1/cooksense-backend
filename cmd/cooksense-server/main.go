1480e17ab76f9b7b647fd35538b2b8332fd88fb7
Alexandre Roy <alexandre.roy-langlois@ifar.ca>
2026-05-02 12:58:46 -0400

    WIP DB

--- cmd/cooksense-server/main.go
+++ cmd/cooksense-server/main.go
@@ -1,13 +1,34 @@
 package main
 
 import (
+	_ "database/sql"
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
 )
+
+var schema = `
+CREATE TABLE IF NOT EXISTS person (
+    first_name text,
+    last_name text,
+    email text
+);`
 
 func main() {
 	fmt.Println("test")
+
+	db, err := sqlx.Connect("postgres", "user=cooksense password=cooksense dbname=cooksense sslmode=disable")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	db.MustExec(schema)
+
+	fmt.Println("Bob")
+
 	server := http.NewServeMux()
 
 	server.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module github.com/mlefebvre1/cooksense-backend
 
 go 1.26.2
+
+require github.com/jmoiron/sqlx v1.4.0
+
+require github.com/lib/pq v1.12.3 // indirect
